49ed5f5c4f80c8d678c8409745d4e3c717ed4402
ElectricNinja315 <suzac314@gmail.com>
2018-07-11 22:56:43 -0700

    example question object

--- IdeologyQuiz.js
+++ IdeologyQuiz.js
@@ -1,3 +1,13 @@
 const cultural = 0;
 const government = 0;
 const economic = 0;
+
+questions = [
+    // example of how question object is structured
+    {
+        text: "",   // the actual text of the question
+        id: ""      // category; either cultural, government, or economic
+    }
+]
+
+// TODO: print each question object 1 by 1, followed by a set of agree/disagree answer choices
